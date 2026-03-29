887c49de004964d56774fd7794cacea73f33cdbf
storylinestudiosofficial <valoip11@gmail.com>
2026-03-29 22:32:56 +0300

    Update server.js

--- server/server.js
+++ server/server.js
@@ -6,7 +6,11 @@ const cloudinary = require('cloudinary').v2;
 const Booking = require('./models/Booking');
 const Portfolio = require('./models/Portfolio');
 require('dotenv').config();
-
+cloudinary.config({ 
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+  api_key: process.env.CLOUDINARY_API_KEY, 
+  api_secret: process.env.CLOUDINARY_API_SECRET 
+});
 const app = express();
 app.use(cors());
 app.use(express.json());
